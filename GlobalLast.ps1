561efd3a05062742228d364e7d1f35da7405b1df
Michael Naumov <mnaoumov@gmail.com>
2015-01-10 11:43:55 +0200

    Reformat

--- GlobalLast.ps1
+++ GlobalLast.ps1
@@ -1,51 +1,54 @@
 # Add Out-Default function to $PROFILE script
 
-# Wrapper for Out-Default that saves the last object written
-# and handles missing commands if the command is a directory
-# or an URL
-#
-function Out-Default `
+function Out-Default
 {
-   [CmdletBinding(ConfirmImpact="Medium")]
-   param(
-       [Parameter(ValueFromPipeline=$true)] `
+    [CmdletBinding(ConfirmImpact = "Medium")]
+    param
+    (
+       [Parameter(ValueFromPipeline = $true)]
        [System.Management.Automation.PSObject] $InputObject
-   )
+    )
 
-   begin
-   {
-     $wrappedCmdlet = $ExecutionContext.InvokeCommand.GetCmdlet(
-       "Out-Default")
-     $sb = { & $wrappedCmdlet @PSBoundParameters }
-     $__sp = $sb.GetSteppablePipeline()
-     $__sp.Begin($pscmdlet)
-   }
-   process {
+    begin
+    {
+        $wrappedCmdlet = $ExecutionContext.InvokeCommand.GetCmdlet("Out-Default")
+        $sb = { & $wrappedCmdlet @PSBoundParameters }
+        $__sp = $sb.GetSteppablePipeline()
+        $__sp.Begin($pscmdlet)
+    }
+    process
+    {
+        $do_process = $true
+        if ($_ -is [System.Management.Automation.ErrorRecord])
+        {
+            if ($_.Exception -is [System.Management.Automation.CommandNotFoundException])
+            {
+                $__command = $_.Exception.CommandName
+                if (Test-Path -Path $__command -PathType container)
+                {
+                    Set-Location $__command
+                    $do_process = $false
+                }
+                elseif ($__command -match '^http://|\.(com|org|net|edu)$')
+                {
+                    if ($matches[0] -ne "http://")
+                    {
+                        $__command = "HTTP://" + $__command
+                    }
 
-       $do_process = $true
-       if ($_ -is [System.Management.Automation.ErrorRecord])
-       {
-           if ($_.Exception -is [System.Management.Automation.CommandNotFoundException])
-           {
-               $__command = $_.Exception.CommandName
-               if (test-path -path $__command -pathtype container)
-               {
-                   set-location $__command
-                   $do_process = $false
-               }
-               elseif ($__command -match '^http://|\.(com|org|net|edu)$')
-               {
-                   if ($matches[0] -ne "http://") { $__command = "HTTP://" + $__command }
-                   [diagnostics.process]::Start($__command)
-                   $do_process = $false
-               }
-           }
-       }
-       if ($do_process)
-       {
-           $global:LAST = $_;
-           $__sp.Process($_)
-       }
-   }
-   end { $__sp.End() }
+                    [System.Diagnostics.Process]::Start($__command)
+                    $do_process = $false
+                }
+            }
+        }
+        if ($do_process)
+        {
+            $global:LAST = $_;
+            $__sp.Process($_)
+        }
+    }
+    end
+    {
+        $__sp.End()
+    }
 }
