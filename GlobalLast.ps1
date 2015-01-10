c7c434bce177f2d4b284aff085451d6fa9efb50e
Michael Naumov <mnaoumov@gmail.com>
2015-01-10 15:20:07 +0200

    Extract Generate-CmdletWrapper with $Begin, $Process, $End

--- GlobalLast.ps1
+++ GlobalLast.ps1
@@ -11,38 +11,23 @@ function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
 
 trap { throw $Error[0] }
 
-function Generate-FormatCmdlet
+function Generate-CmdletWrapper
 {
     param
     (
-        [string] $CmdletName
+        [string] $CmdletName,
+        [ScriptBlock] $Begin,
+        [ScriptBlock] $Process,
+        [ScriptBlock] $End
     )
 
     $command = Get-Command -Name $CmdletName -CommandType Cmdlet
     $metadata = New-Object -TypeName System.Management.Automation.CommandMetaData -ArgumentList @($command)
 
     $functionText = [System.Management.Automation.ProxyCommand]::Create($metadata)
-    $functionText = $functionText -replace "begin\s*\{\s*try\s*\{", ("`$0`n" +
-@'
-$tempLast = @()
-'@)
-    $functionText = $functionText -replace "process\s*\{\s*try\s*\{", ("`$0`n" +
-@'
-$tempLast += @(, $$_)
-'@)
-    $functionText = $functionText -replace "end\s*\{\s*try\s*\{", ("`$0`n" +
-@'
-        if ($tempLast.Length -eq 1)
-        {
-            $Global:LastResult = $tempLast[0]
-        }
-        else
-        {
-            $Global:LastResult = $tempLast
-        }
-
-        $Global:LastResultCmdletWasFormat = $true
-'@)
+    $functionText = $functionText -replace "begin\s*\{\s*try\s*\{", ("`$0`n" + ("$Begin" -replace '\$', '$$$$'))
+    $functionText = $functionText -replace "process\s*\{\s*try\s*\{", ("`$0`n" + ("$Process" -replace '\$', '$$$$'))
+    $functionText = $functionText -replace "end\s*\{\s*try\s*\{", ("`$0`n" + ("$End" -replace '\$', '$$$$'))
 
     Set-Item -Path "Function:Global:$CmdletName" -Value $functionText
 }
@@ -91,4 +76,29 @@ function Out-Default
 
 Get-Command -Verb Format -Module Microsoft.PowerShell.Utility | `
     Select -ExpandProperty Name | `
-    ForEach-Object -Process { Generate-FormatCmdlet -CmdletName $_ }
+    ForEach-Object -Process `
+        {
+            Generate-CmdletWrapper `
+                -CmdletName $_ `
+                -Begin `
+                    {
+                        $tempLast = @()
+                    } `
+                -Process `
+                    {
+                        $tempLast += @(, $_)
+                    } `
+                -End `
+                    {
+                        if ($tempLast.Length -eq 1)
+                        {
+                            $Global:LastResult = $tempLast[0]
+                        }
+                        else
+                        {
+                            $Global:LastResult = $tempLast
+                        }
+
+                        $Global:LastResultCmdletWasFormat = $true
+                    }
+        }
