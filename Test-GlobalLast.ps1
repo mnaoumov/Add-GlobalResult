d65d341ccd51d7e535e3d2ecf58e763a26df44bc
Michael Naumov <mnaoumov@gmail.com>
2015-01-10 12:28:06 +0200

    Add Test-GlobalLast

---
+++ Test-GlobalLast.ps1
@@ -0,0 +1,31 @@
+﻿#requires -version 2.0
+
+[CmdletBinding()]
+param
+(
+)
+
+$script:ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+function PSScriptRoot { $MyInvocation.ScriptName | Split-Path }
+
+trap { throw $Error[0] }
+
+if (Test-Path Function:\Out-Default)
+{
+    Remove-Item -Path Function:\Out-Default
+}
+
+if (Test-Path Variable:\LAST)
+{
+    Remove-Item -Path Variable:\LAST
+}
+
+. "$(PSScriptRoot)\GlobalLast.ps1"
+
+42 | Out-Default
+
+if ($Global:LAST -ne 42)
+{
+    throw "`$Global:LAST extected to be 42, actual: $Global:LAST"
+}
